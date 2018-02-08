3e5951ce10bd3681fda24f152e6c41ebe3a21e63
Hedviga <hedv0147@edu.eal.dk>
2018-02-08 17:58:24 +0100

    a

---
+++ Ex2Sokets/Client/Client.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        private string rmsg = "";
+        private string wmsg = "";
+        static void Main(string[] args)
+        {
+            Program Client = new Program();
+            Client.Run();
+        }
+
+        private void Run()
+        {
+            TcpClient client = new TcpClient("localhost", 5000);
+            NetworkStream stream = client.GetStream();
+            StreamReader read = new StreamReader(stream);
+            StreamWriter write = new StreamWriter(stream);
+            write.AutoFlush = true;
+
+            do
+            {
+                rmsg = read.ReadLine();
+                Console.WriteLine("Server: " + rmsg);
+                wmsg = Console.ReadLine();
+                write.WriteLine(wmsg);
+
+            }
+            while (wmsg != "exit");
+            Console.ReadKey();
+        }
+        
+        
+        
+    }
+    
+}
---
+++ Ex2Sokets/Ex2Sokets/Server.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace Ex2Sokets
+{
+    class Server
+    {
+        static bool running;
+        private string wmsg = "";
+        private string rmsg = "";
+        static void Main(string[] args)
+        {
+            running = true;
+            Server pr = new Server();
+            pr.Run();
+            
+            
+        }
+        public void Run()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 5000);
+            listener.Start();
+            while (running)
+            {
+                Socket client = listener.AcceptSocket();
+                new Thread(new ThreadStart(() => ThreadRun(client))).Start();
+                //Thread clientThread = new Thread(() => ThreadRun(client));
+                //clientThread.Start();
+            }
+        }
+        public void ThreadRun(Socket client)
+        {
+            NetworkStream stream = new NetworkStream(client);
+            StreamWriter writer = new StreamWriter(stream);
+            StreamReader reader = new StreamReader(stream);
+            writer.AutoFlush = true;
+            writer.WriteLine("Ready!!!");
+            do
+            {
+                rmsg = reader.ReadLine();
+                if (rmsg == "time")
+                {
+                    wmsg = DateTime.Now.ToString("h:mm:ss tt");
+                    writer.WriteLine(wmsg);
+                }
+                else
+                {
+                    wmsg = "Unknown command";
+                    writer.WriteLine(wmsg);
+                }
+
+            } while (running);
+            Console.ReadKey();
+
+        }
+    }
+}
